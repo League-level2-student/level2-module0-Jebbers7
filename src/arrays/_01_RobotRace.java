f1cff6782f4386f373127bbe68f8d2176f38f8c7
league <league@imac-55.attlocal.net>
2021-08-23 18:25:14 -0700

    robotrace in progress

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,5 +1,9 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
@@ -8,21 +12,25 @@ public class _01_RobotRace {
 		
 	//2. create an array of 5 robots.
 	Robot[] robots = new Robot[5];
+	int xPosition = 100;
 	//3. use a for loop to initialize the robots.
 	for(int i = 0; i < robots.length; i++) {
 		robots[i] = new Robot();
-		int xPosition = 0;
-		robots[i].setY(0);
+		robots[i].setY(500);
 		robots[i].setX(xPosition);
-		xPosition += 25;
+		xPosition += 100;
 	}
 	//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+	Random ran = new Random();
 	
+	for(int i = 0; i < robots.length; i++) {
+		robots[i].move(ran.nextInt(50));
+	}
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+	
 	//7. declare that robot the winner and throw it a party!
 
 	//8. try different races with different amounts of robots.
